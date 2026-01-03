c6c689eece524dea072baaa93df088b1581495be
WS01 <adibogdan2004@gmail.com>
2026-01-03 13:57:54 +0200

    add generateMatrix.cpp for data

---
+++ jetson/generateMatrix.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+#define MATRIX_NO 100
+#define MATRIX_SIZE 2048
+#define MIN_VALUE 0
+#define MAX_VALUE 1024
+
+ofstream fout("matrix.txt");
+
+int main()
+{
+    for (int n = 0; n < MATRIX_NO; n++) 
+    {
+        for (int i = 0; i < MATRIX_SIZE; i++) 
+        {
+            for (int j = 0; j < MATRIX_SIZE; j++) 
+            {
+                int value = MIN_VALUE + rand() % (MAX_VALUE - MIN_VALUE + 1);
+                fout << value << " ";
+            }
+            fout << "\n";
+        }
+        fout << "\n";
+        cout<<"Done "<<n<<endl;
+    }
+    return 0;
+}
